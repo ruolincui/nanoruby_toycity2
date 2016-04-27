cad97409d38372668d22742cb15ba2029935f232
Ruolin Cui <beier24@gmail.com>
2016-04-26 22:18:07 -0400

    define brand methods

--- app.rb
+++ app.rb
@@ -59,27 +59,24 @@ def create_report
 		puts ""
 	end
 	print_ascii_brand
-	get_uniq_brand
-	/
+	get_uniq_brand()
 	$unique_brand.each do |brand|
+		puts brand
 		puts "***********************************"
-		$brands_hash = $products_hash['items'].select{ |toy| toy['brand'] == brand}
-		$brands_hash.each do |item|
-			puts "Total Stock of #{brand}: #{get_brand_stock(item)}"
-			puts "Average Price of #{brand}: $#{get_average_price_brand(item)}"
-			puts "Total Revenue of #{brand}: $#{get_revenue_brand(item)}"
-			puts ""
-		end
+		brands_hash = $products_hash['items'].select{ |toy| toy['brand'] == brand}
+		puts "Total Stock of #{brand}: #{get_brand_stock(brands_hash)}"
+		puts "Average Price of #{brand}: $#{get_average_price_brand(brands_hash)}"
+		puts "Total Revenue of #{brand}: $#{get_revenue_brand(brands_hash)}"
+		puts ""	
 	end
-	/
 end
 
 
 
 # For each product in the data set:
 	# Print the name of the toy
-def get_title(toy)
-	return toy['title']
+def get_title(x)
+	return x['title']
 end
 
 	# Print the retail price of the toy
@@ -125,37 +122,39 @@ end
                                   
 # For each brand in the data set:
 	# Print the name of the brand
-def get_uniq_brand(toy)
+def get_uniq_brand()
 	$unique_brand = $products_hash['items'].map{ |item| item['brand'] }.uniq
-	return $unique_brand
 end
 
 	# Count and print the number of the brand's toys we stock
-def get_brand_stock(item)
+def get_brand_stock(brands_hash)
 	total_stock_brand = 0
-	total_stock_brand += item['stock']
+	brands_hash.each do |item|
+		total_stock_brand += item['stock']
+	end
 	return total_stock_brand
 end
 
 	# Calculate and print the average price of the brand's toys
-def get_average_price_brand(item)
+def get_average_price_brand(brands_hash)
 	total_price_brand = 0
     average_price_brand = 0
-    total_price_brand += item['full-price'].to_f
-    average_price_brand = (total_price_brand / $brands_hash.length).round(2)
+    brands_hash.each do |item|
+    	total_price_brand += item['full-price'].to_f
+    	average_price_brand = (total_price_brand / brands_hash.length).round(2)
+    end
     return average_price_brand
 end
 
 	# Calculate and print the total sales volume of all the brand's toys combined
-def get_revenue_brand(item)
+def get_revenue_brand(brands_hash)
 	total_revenue_brand = 0
-	item['purchases'].each do |purchase|
-		total_revenue_brand += purchase['price']
+	brands_hash.each do |item|
+		item['purchases'].each do |purchase|
+			total_revenue_brand += purchase['price']
+		end
 	end
-	return total_revenue_brand
+	return total_revenue_brand.round(2)
 end
-
-
-
 
 start
